7908e86e615f792e75071a319543599585fa6e20
Kyle Carow <kylecarow@gmail.com>
2026-07-31 22:42:27 -0600

    build-index working

--- tools/build-index/src/main.rs
+++ tools/build-index/src/main.rs
@@ -1,4 +1,3 @@
-use anyhow::{Context, ensure};
 use clap::Parser;
 use std::path::PathBuf;
 
@@ -38,13 +37,13 @@ fn parse_args() -> anyhow::Result<(u32, PathBuf, PathBuf)> {
     let schema_version = cli.schema;
 
     let schema_dir = std::path::absolute(&cli.root_dir)?.join(format!("v{}", schema_version));
-    ensure!(
+    anyhow::ensure!(
         schema_dir.exists(),
         "directory not found at schema v{} path: {:?}",
         schema_version,
         schema_dir
     );
-    ensure!(
+    anyhow::ensure!(
         schema_dir.is_dir(),
         "resolved schema v{} path is not a directory: {:?}",
         schema_version,
@@ -53,14 +52,14 @@ fn parse_args() -> anyhow::Result<(u32, PathBuf, PathBuf)> {
     let schema_dir = std::fs::canonicalize(schema_dir)?;
 
     let output_file_path = {
-        let candidate = std::path::PathBuf::from(&cli.output_file);
+        let candidate = PathBuf::from(&cli.output_file);
         if candidate.is_absolute() {
             candidate
         } else {
             schema_dir.join(candidate)
         }
     };
-    ensure!(
+    anyhow::ensure!(
         output_file_path.extension() == Some(std::ffi::OsStr::new("jsonl")),
         "output file path must have a .jsonl extension: {:?}",
         output_file_path
@@ -69,13 +68,8 @@ fn parse_args() -> anyhow::Result<(u32, PathBuf, PathBuf)> {
     Ok((schema_version, schema_dir, output_file_path))
 }
 
-fn scan_file_paths(scan_root_dir: &std::path::Path) -> anyhow::Result<Vec<PathBuf>> {
-    let mut file_paths = Vec::new();
-    collect_relative_file_paths(scan_root_dir, scan_root_dir, &mut file_paths)?;
-    Ok(file_paths)
-}
-
-fn collect_relative_file_paths(
+/// Recursively scans & collects all paths relative to `scan_root_dir`.
+fn scan_file_paths(
     scan_root_dir: &std::path::Path,
     current_dir: &std::path::Path,
     file_paths: &mut Vec<PathBuf>,
@@ -84,7 +78,7 @@ fn collect_relative_file_paths(
         let entry = entry?;
         let path = entry.path();
         if path.is_dir() {
-            collect_relative_file_paths(scan_root_dir, &path, file_paths)?;
+            scan_file_paths(scan_root_dir, &path, file_paths)?;
         } else {
             let relative_path = path.strip_prefix(scan_root_dir)?;
             file_paths.push(relative_path.to_path_buf());
@@ -94,50 +88,56 @@ fn collect_relative_file_paths(
     Ok(())
 }
 
-fn get_date_from_path(path: &std::path::Path) -> Option<String> {
-    Some("".to_string()) // Placeholder implementation
-}
-
 fn main() -> anyhow::Result<()> {
     let (schema_version, scan_root_dir, output_file_path) = parse_args()?;
+    let schema_prefix = PathBuf::from(format!("v{}", schema_version));
+
     println!("Building index from directory {}", scan_root_dir.display());
 
     // Scan for vehicle files
-    let file_paths = scan_file_paths(&scan_root_dir)?;
+    let mut file_paths = Vec::new();
+    scan_file_paths(&scan_root_dir, &scan_root_dir, &mut file_paths)?;
+    println!("Found {} files", file_paths.len());
 
     // Write to file
     let mut output_file = std::fs::File::create(&output_file_path)?;
     match schema_version {
         1 => {
             let mut entries = Vec::with_capacity(file_paths.len());
-            for path in &file_paths {
+            for relative_file_path in &file_paths {
+                let schema_file_path = schema_prefix.join(relative_file_path);
+                // Skip non-UTF8 paths
+                let Some(schema_file_path_str) = schema_file_path.to_str() else {
+                    println!("Skipping non-UTF8 path: {:?}", schema_file_path);
+                    continue;
+                };
+                // Skip non-YAML files
+                let Some(schema_path_without_ext) = schema_file_path_str.strip_suffix(".yaml")
+                else {
+                    println!("Skipping non-YAML file: {:?}", schema_file_path_str);
+                    continue;
+                };
                 // Skip files that don't parse as valid VehicleSchemaV1 paths
-                if path
-                    .to_str()
-                    .with_context(|| format!("failed to convert path to string: {:?}", path))?
-                    .strip_suffix(".yaml")
-                    .with_context(|| format!("failed to strip .yaml suffix from path: {:?}", path))?
+                if schema_path_without_ext
                     .parse::<fastsim_schema::VehicleSchemaV1>()
                     .is_err()
                 {
                     continue;
-                };
-                // Create an IndexEntryV1
-                let date_added = get_date_from_path(path);
-                let entry = fastsim_schema::IndexEntryV1::new(
-                    path.to_string_lossy(),
-                    date_added.unwrap_or_default(),
-                )?;
-                entries.push(entry);
+                }
+                // Create index entry
+                let index_entry = schema_file_path_str.parse::<fastsim_schema::IndexEntryV1>()?;
+                entries.push(index_entry);
             }
+            println!("Created {} index entries", entries.len());
+
             // Sort entries alphabetically by path
             entries.sort_by(|a, b| a.path.cmp(&b.path));
+
             // Write entries to output file in JSONL format
             fastsim_schema::write_jsonl_v1(&mut output_file, &entries)?;
         }
         _ => anyhow::bail!("unsupported schema version: {}", schema_version),
     };
-
     println!("Index saved to {}", output_file_path.display());
 
     Ok(())
